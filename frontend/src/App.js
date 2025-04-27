510c00b5a0227920a6bfc9b53d5330b5525ce252
Jagriti <jagriti.singh22@st.niituniversity.in>
2025-04-27 20:22:08 +0530

    adding dataset of images and images of frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,39 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import HomePage from './HomePage';
 import LoginPage from './LoginPage';
 import RegisterPage from './RegisterPage';
 import SessionOverPage from './SessionOverPage';
+import Header from "./components/Header"; 
+import Home from "./components/Home"; 
 import Tutorial from "./components/Tutorial";
 
 function App() {
   return (
     <Router>
+      <Main />
+    </Router>
+  );
+}
+
+function Main() {
+  const location = useLocation();
+  const showHeader = location.pathname === "/components/Home" || location.pathname === "/components/Tutorial";
+
+  return (
+    <>
+      {showHeader && <Header />}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/session-over" element={<SessionOverPage />} />
+        <Route path="/components/Home" element={<Home />} /> 
         <Route path="/components/Tutorial" element={<Tutorial />} /> 
       </Routes>
-    </Router>
+    </>
   );
 }
 
 export default App;
+
