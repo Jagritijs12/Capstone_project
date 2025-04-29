064797af7671a58a56f382e3f57f76d589549b1e
ArchanaAnto <archana.anto22@st.niituniversity.in>
2025-04-30 04:10:39 +0530

    model components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,66 +6,36 @@ import RegisterPage from './components/RegisterPage';
 import SessionOverPage from './components/SessionOverPage';
 import Tutorial from "./components/Tutorial";
 import Home from "./components/Home";
-//import Card from "./components/Card";
 import Header from "./components/Header";
-import Contact from './components/Contact';
-import AnalyzeLogs from "./components/AnalyzeLogs";
-import AnalyzeImages from "./components/AnalyzeImages";
-import AnalyzeDocuments from "./components/AnalyzeDocuments";
-// 
+import AnalyzeLogs from "./components/AnalyzeLogs";        // <-- NEW
+import AnalyzeImages from "./components/AnalyzeImages";    // <-- NEW
+import AnalyzeDocuments from "./components/AnalyzeDocuments"; // <-- NEW
+
+// New model components
+import ImagePredictor from "./components/ImagePredictor";
+import DocPredictor from "./components/DocPredictor";
+import LogPredictor from "./components/LogPredictor";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/register' element={<RegisterPage />} />
-        <Route path='/session-over' element={<SessionOverPage />} />
-
-        <Route path='/home' element={
-          <>
-            <Header />
-            <Home />
-          </>
-        } />
-        <Route path='/tutorial' element={
-          <>
-            <Header />
-            <Tutorial />
-          </>
-        } />
-        <Route path='/analyze/logs' element={
-          <>
-            <Header />
-            <AnalyzeLogs />
-          </>
-        } />
-        <Route path='/analyze/images' element={
-          <>
-            <Header />
-            <AnalyzeImages />
-          </>
-        } />
-        <Route path='/analyze/documents' element={
-          <>
-            <Header />
-            <AnalyzeDocuments />
-          </>
-        } />
-        <Route path='/contact' element={
-          <>
-            <Header />
-            <Contact />
-          </>
-        } />
-      </Routes>
+      <Header />
+      <div className="app">
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/session-over' element={<SessionOverPage />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/tutorial" element={<Tutorial />} />
+          {/* New routes */}
+          <Route path="/analyze/logs" element={<LogPredictor />} /> {/* New Route */}
+          <Route path="/analyze/images" element={<ImagePredictor />} /> {/* New Route */}
+          <Route path="/analyze/documents" element={<DocPredictor />} /> {/* New Route */}
+        </Routes>
+      </div>
     </Router>
   );
 }
 
-
 export default App;
-
-
-
