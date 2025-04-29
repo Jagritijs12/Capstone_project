038c48adc001bf8deed35a8ccf05fe9d436f932d
Ahana99Nair <ahana.nair22@st.niituniversity.in>
2025-04-29 14:20:50 +0530

    Update in App.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,17 +13,18 @@ import Header from "./components/Header";
 function App() {
   return (
     <Router>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/session-over' element={<SessionOverPage />} />
+      </Routes>
       <div className="app">
         <Header />
-
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/session-over" element={<SessionOverPage />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/tutorial" element={<Tutorial />} />
-        </Routes>
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/tutorial" element={<Tutorial />} />
+          </Routes>
       </div>
     </Router>
   );
