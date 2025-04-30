4e50d79e359d81ec4625b2419b98fa8f967604bf
Ahana99Nair <ahana.nair22@st.niituniversity.in>
2025-04-30 15:42:45 +0530

    Updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-//App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
@@ -8,34 +7,55 @@ import SessionOverPage from './components/SessionOverPage';
 import Tutorial from "./components/Tutorial";
 import Home from "./components/Home";
 import Header from "./components/Header";
-import Contact from "./components/Contact";
-import AnalyzeLogs from "./components/AnalyzeLogs";        // <-- NEW
+//import AnalyzeLogs from "./components/AnalyzeLogs";        // <-- NEW
 //import AnalyzeImages from "./components/AnalyzeImages";    // <-- NEW
 //import AnalyzeDocuments from "./components/AnalyzeDocuments"; // <-- NEW
 
 // New model components
-//import ImagePredictor from "./components/ImagePredictor";
-//import DocPredictor from "./components/DocPredictor";
-//import LogPredictor from "./components/LogPredictor";
+import ImagePredictor from "./components/ImagePredictor";
+import DocPredictor from "./components/DocPredictor";
+import LogPredictor from "./components/LogPredictor";
 
 function App() {
   return (
     <Router>
-      <Header />
-      <div className="app">
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/register' element={<RegisterPage />} />
-          <Route path='/session-over' element={<SessionOverPage />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/tutorial" element={<Tutorial />} />
-          {/* New routes */}
-          <Route path="/analyze/logs" element={<AnalyzeLogs />} /> {/* New Route */}
-          <Route path="/analyze/images" element={<ImagePredictor />} /> {/* New Route */}
-          <Route path="/analyze/documents" element={<DocPredictor />} /> {/* New Route */}
-        </Routes>
-      </div>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/session-over' element={<SessionOverPage />} />
+
+        <Route path='/home' element={
+          <>
+            <Header />
+            <Home />
+          </>
+        } />
+        <Route path='/tutorial' element={
+          <>
+            <Header />
+            <Tutorial />
+          </>
+        } />
+        <Route path='/analyze/logs' element={
+          <>
+            <Header />
+            <LogPredictor />
+          </>
+        } />
+        <Route path='/analyze/images' element={
+          <>
+            <Header />
+            <ImagePredictor />
+          </>
+        } />
+        <Route path='/analyze/documents' element={
+          <>
+            <Header />
+            <DocPredictor />
+          </>
+        } />
+      </Routes>
     </Router>
   );
 }
